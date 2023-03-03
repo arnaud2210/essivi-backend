b61ff2e0cd01a98fcc4052a11ff161e7364c1737
ADJIMON Arnaud Fouayéyêmi <84155073+arnaud2210@users.noreply.github.com>
2023-03-03 02:45:56 +0000

    adding activation of account

--- routers/authentication.py
+++ routers/authentication.py
@@ -25,4 +25,4 @@ def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(
                             detail=f"Incorrect password")
 
     access_token = create_access_token(data={"sub": user.login})
-    return {"access_token": access_token, "token_type": "bearer", "user_id": user.idUser, "user_login": user.login, "role_id": user.role_id, "label": role.label}
+    return {"access_token": access_token, "token_type": "bearer", "user_id": user.idUser, "user_login": user.login, "role_id": user.role_id, "label": role.label, "is_active": user.is_active}
