f384bb3cdd8ae4abbf6cc4cc1494af591b13efc6
ADJIMON Arnaud Fouayéyêmi <84155073+arnaud2210@users.noreply.github.com>
2023-03-05 22:48:20 +0000

    update jwt timedelta

--- settings/jwt.py
+++ settings/jwt.py
@@ -15,7 +15,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
