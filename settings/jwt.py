2f580867392349f4c8b46f0c4f549fec85f2ed17
ADJIMON Arnaud Fouayéyêmi <84155073+arnaud2210@users.noreply.github.com>
2023-03-05 22:52:08 +0000

    update jwt token expiration

--- settings/jwt.py
+++ settings/jwt.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from settings import schemas
 from dotenv import load_dotenv
@@ -15,7 +15,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
