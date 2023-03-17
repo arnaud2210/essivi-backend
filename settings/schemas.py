71e0502b974c7d54632caf9ec7ea7f8c64995525
ADJIMON Arnaud Fouayéyêmi <84155073+arnaud2210@users.noreply.github.com>
2023-03-17 09:04:25 +0000

    update ordered schema

--- settings/schemas.py
+++ settings/schemas.py
@@ -93,7 +93,7 @@ class UpdateUserAccount(BaseModel):
 
 class Ordered(BaseModel):
     ordered_quantity: int
-    ordered_date: datetime
+    # ordered_date: datetime
     customer_id: int
     product_id: int
 
